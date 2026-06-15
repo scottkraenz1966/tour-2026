7c89d38059a5d5a737a4e17811666c2b08d3103d
Scott Kraenzlein <scott@wskartists.com>
2026-06-15 15:45:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'tour2026-v1';
+const ASSETS = [
+  './bikepacking-itinerary-aug2026.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
